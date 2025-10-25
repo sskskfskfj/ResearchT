a8dfb892686a477dc47ca5fb4460072a61149ff4
sskskfskfj <dongkyun7717@gmail.com>
2025-10-26 00:06:41 +0900

    chore: facebook/nllb-200-distilled-1.3B model test

--- model/models.py
+++ model/models.py
@@ -6,7 +6,7 @@ HUGGINGFACE_TOKEN = secret.huggingface_token
 
 
 def init_teacher_model(model_name : str = "facebook/nllb-200-distilled-1.3B"):
-    tokenizer = AutoTokenizer.from_pretrained(model_name, token = HUGGINGFACE_TOKEN, src_lang="eng_Latn")
+    tokenizer = AutoTokenizer.from_pretrained(model_name, token = HUGGINGFACE_TOKEN, src_lang = "eng_Latn")
     model = AutoModelForSeq2SeqLM.from_pretrained(model_name, token = HUGGINGFACE_TOKEN)
     model.eval()
     
@@ -16,8 +16,8 @@ def init_teacher_model(model_name : str = "facebook/nllb-200-distilled-1.3B"):
 if __name__ == "__main__":
     tokenizer, model = init_teacher_model()
 
-    encoded = tokenizer("I love machine learning", return_tensors="pt", padding=True, truncation=True)
-    generated = model.generate(**encoded, forced_bos_token_id=tokenizer.convert_tokens_to_ids("kor_Hang"))
+    encoded = tokenizer("I love machine learning", return_tensors = "pt", padding = True, truncation = True)
+    generated = model.generate(**encoded, forced_bos_token_id = tokenizer.convert_tokens_to_ids("kor_Hang"))
 
-    translated = tokenizer.decode(generated[0], skip_special_tokens=True)
+    translated = tokenizer.decode(generated[0], skip_special_tokens = True)
     print(translated)
